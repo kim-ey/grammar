fb2bcceef4cd9a07cc3e9c411a70272bf45a94a4
kim-ey <kim_55ey@yahoo.co.jp>
2022-01-22 12:00:16 +0900

    21 study

---
+++ bootstrap/template/3.iPortfolio/test.js
@@ -0,0 +1,14 @@
+console.log('DOM:', document)
+
+/**
+ * test function
+ */
+function left() {
+    let a = document.querySelector('#test')
+    a.innerHTML = 'TEST!!'
+}
+
+function right() {
+    let a = document.querySelector('#test')
+    a.innerHTML = '私について'
+}
